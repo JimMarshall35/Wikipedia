8678c7951e50807b727c457a24adcf1cb342a505
Jim Marshall <jimmarshall35@gmail.com>
2020-01-18 19:32:24 +0000

    coments added

--- philosphy scraper.py
+++ philosphy scraper.py
@@ -9,30 +9,33 @@ print('╔═╗╦ ╦╦╦  ╔═╗╔═╗╔═╗╔═╗╦ ╦╦ �
 print('╠═╝╠═╣║║  ║ ║╚═╗║ ║╠═╝╠═╣╚╦╝  ╚═╗║  ╠╦╝╠═╣╠═╝║╣ ╠╦╝')
 print('╩  ╩ ╩╩╩═╝╚═╝╚═╝╚═╝╩  ╩ ╩ ╩   ╚═╝╚═╝╩╚═╩ ╩╩  ╚═╝╩╚═')
 print()
-def getFirstLink(pList):
+def getFirstLink(pList): # takes a lost of p tags
     allow = True
-    for p in pList:
-        children = p.contents
+    for p in pList:           # iterate through p tags
+        print("P: " + p.name)
+        children = p.contents # iterate through each p tags contents
         for child in children:
-            if "(" in child:
-                allow = False
-            if ")" in child:
-                allow = True
-            #print(child.name)
-            if child.name == "i" or child.name == "b" and allow:
-                #print("ok")
+            if "(" in child:  #
+                allow = False #
+                print(allow)  # disallow links in brackets
+            if ")" in child:  #
+                allow = True  #
+                print(allow)  #
+            print(child)
+            if child.name == "i" or child.name == "b" and allow: # allow for bold and italic links
+                print("ok")
                 if(len(child.contents)) == 0:
                     continue
-                #print(child.contents[0])
+                print(child.contents[0])
                 if child.contents[0].name == "a":
                     if 'title' in child.attrs:
-                        if child.contents[0]['title'][:4] == 'wikt':
+                        if child.contents[0]['title'][:4] == 'wikt': # prevent links to wiktionary
                             continue
-                        link = child.contents[0].get("href")
+                        link = child.contents[0].get("href") # get hyperlink
                         if link != None:
                             return "https://en.wikipedia.org"+link
 
-            if child.name == "a" and allow:
+            if child.name == "a" and allow:          # non bold or italic links
                 if 'title' in child.attrs:
                     if child['title'][:4] == 'wikt':
                         continue
@@ -45,13 +48,15 @@ def connect(string):
         src = requests.get(string).text
         return src
     except:
-        connect(string)
-G = nx.Graph()
+        connect(string)   # if cant connect then don't throw an error just try again
+        
+G = nx.Graph() # create networkx graph object
 num = input("how many repetitions? ")
 print()
+visitedtitles = []
 for x in range(int(num)):
     src = connect("https://en.wikipedia.org/wiki/Special:Random")
-    visitedtitles = []
+    #visitedtitles = []
     firstpage = BeautifulSoup(src,'lxml').title.string
     philosophy = False
     search = True
@@ -61,15 +66,15 @@ for x in range(int(num)):
         print("----------* " + "LINK " + str(len(visitedtitles)) + " run: "+ str(x+1) +" / "+num+ " *----------" )
         print()
         soup = BeautifulSoup(src, "lxml")
-        for title in visitedtitles:
+        for title in visitedtitles: # if page has been visited exit while lop
             if title.string == soup.title.string:
                 search = False
-        if soup.title.string == "Philosophy - Wikipedia":
+        if soup.title.string == "Philosophy - Wikipedia": # set philosophy visited flag to true if philosophy visited
             philosophy = True
             
-        visitedtitles.append(soup.title.string)
+        visitedtitles.append(soup.title.string) 
 
-        print("TITLE: " + soup.title.string[:-12])
+        print("TITLE: " + soup.title.string[:-12]) # get a sub string to remove the "- Wikipedia" bit from the page titles
         if G.has_node(soup.title.string[:-12]) == False:
             G.add_node(soup.title.string[:-12])
         if previousnode != None:
@@ -77,18 +82,18 @@ for x in range(int(num)):
                 G.add_edge(previousnode, soup.title.string[:-12])
         previousnode = soup.title.string[:-12]
 
-        mwparseroutput = soup.find('div', class_='mw-parser-output')
+        mwparseroutput = soup.find('div', class_='mw-parser-output') # this div tag contains the article
 
         #soup2 = BeautifulSoup(t, "lxml")
-        pList = mwparseroutput.find_all('p')
-        firstlink = getFirstLink(pList)
+        pList = mwparseroutput.find_all('p') # find all paragraphs in article
+        firstlink = getFirstLink(pList)      # get the first link on the page
         if firstlink != None:
             print("FIRST LINK: "+firstlink)
             src = connect(firstlink)
             print()
-        else:
-            write = False
-            break
+        #else:
+         #   write = False
+          #  break
     print()
     print(str(len(visitedtitles)) + " PAGES VISITED")
     print("LOOP " + visitedtitles[len(visitedtitles)-1].string)
